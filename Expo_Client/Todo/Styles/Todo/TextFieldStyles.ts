1824aa170de17a08b61f6d38e35068b6840ad91d
sagarbhusal01 <sagar.bhu777@gmail.com>
2024-07-01 21:13:59 +0545

    text field position changed

---
+++ Expo_Client/Todo/Styles/Todo/TextFieldStyles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+import {
+  BACKGROUND,
+  BUTTONS,
+  HEIGHT,
+  INFORMATION,
+  WHITE,
+  WIDTH,
+  TEXTFEILD,
+} from "../../Todo/Global";
+
+export const TextFieldStyles = StyleSheet.create({
+  Container: {
+    width: WIDTH * 0.88,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: 15,
+  },
+  TextInput: {
+    width: "100%",
+    height: 45,
+    backgroundColor: TEXTFEILD,
+    borderRadius: 8,
+    paddingLeft: 15,
+    color: WHITE,
+  },
+});
