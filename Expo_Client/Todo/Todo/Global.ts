60647fdada6d87e0af740013dd3bfcb7ba0db9da
sagarbhusal01 <sagar.bhu777@gmail.com>
2024-07-01 21:14:36 +0545

    changed the screen size to the window size

--- Expo_Client/Todo/Todo/Global.ts
+++ Expo_Client/Todo/Todo/Global.ts
@@ -1,20 +1,19 @@
-import { Dimensions,StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 
 export const BACKGROUND = "#373737";
 export const PLACEHOLDER = "#989898";
 export const BUTTONS = "#868686";
 export const TEXTFEILD = "#575757";
 export const INFORMATION = "#505050";
-export const IDEATAG="#F6A23D"
+export const IDEATAG = "#F6A23D";
 export const EMERGENCYTAG = "tomato";
-export const WIDTH = Dimensions.get("screen").width;
+export const WIDTH = Dimensions.get("window").width;
 export const WHITE = "#ffffff";
-export const HEIGHT = Dimensions.get("screen").height;
+export const HEIGHT = Dimensions.get("window").height;
 export const GETALLTODOURL = ":1917/getalltodo";
 export const ADDTODOURL = ":1917/addtodo";
 export const TOGGLETODOURL = ":1917/togglecompleted";
 export const DELETETODOURL = ":1917/deletetodo";
-
 
 export const GlobalStyles = StyleSheet.create({
   Container: {
